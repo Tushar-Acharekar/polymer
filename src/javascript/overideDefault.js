1d03e7660f0474213510c6f27f79c86113423065
acharekart <acharekart@gmail.com>
2017-02-11 04:32:58 -0500

    Path injection

---
+++ src/javascript/overideDefault.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){	
+
+	adjustment();
+
+	$(window).resize(function() {adjustment();});
+});
+
+
+function adjustment(){
+/* 	var finalHeight = $(".main_header").position().top + $(".main_header").outerHeight(); 
+	var screenWidth = screen.width;
+ 	$(".mainBody").css({"position": "relative", "top":finalHeight}); 
+	$(".mdl-layout__drawer").css({"position": "relative", "top":finalHeight}); */
+}
+
